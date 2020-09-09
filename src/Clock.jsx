e515f2021a6946d9c8cac8f933d7a2c81c210609
June Ang <June Ang>
2020-09-09 10:13:38 +0800

    Add countdown calculation

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -7,11 +7,30 @@ class Clock extends Component {
     this.state = { day: 0, hour: 0, min: 0, sec: 0 };
   }
 
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  getTimeUntil = (deadline) => {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const sec = Math.floor((time / 1000) % 60);
+    const min = Math.floor((time / 1000 / 60) % 60);
+    const hour = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const day = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    this.setState({
+      day,
+      hour,
+      min,
+      sec,
+    });
+  };
+
   render() {
     return (
       <div>
         <div className="clock clock-day">{this.state.day} Days</div>
-        <div className="clock clock-hour">{this.state.min} Hours</div>
+        <div className="clock clock-hour">{this.state.hour} Hours</div>
         <div className="clock clock-min">{this.state.min} Minutes</div>
         <div className="clock clock-seconds">{this.state.sec} Seconds</div>
       </div>
